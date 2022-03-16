2a26523344219353c8c1e7870adf2ad620e7c191
J Semidey <mtsemidey@gmail.com>
2022-03-16 13:47:50 -0400

    tasks now draggable/sortable, updates localstorage when moved

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,6 +190,66 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// sorting tasks: select by class, set to sortable (makes every el with matching class into sortable list)
+// connectWith to any other lists matching selector class
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  // helper: makes a copy of the dragged element and moves copy instead, prevents click events from triggering on original element
+  helper: "clone",
+  // activate/deactivate trigger once for all connected lists as soon as dragging starts and stops
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  // over/out trigger when a dragged item enters or leaves a connected list
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  // update triggers when the contents of a list have changed (re-ordered, removed, added, etc)
+  update: function(event) {
+    // array to store task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to temp array as object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] =  tempArr;
+    saveTasks();
+
+  }
+});
+
+
+
 // load tasks for the first time
 loadTasks();
 
